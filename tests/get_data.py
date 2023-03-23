a47f80767b19dadf54b356fcd5a2503c281cdb0b
Chenying Zhao <chenyzh@seas.upenn.edu>
2023-03-23 18:47:26 -0400

    add in pytests: pull docker image + create container dataset; add pytest to circleci tests;

--- tests/get_data.py
+++ tests/get_data.py
@@ -2,17 +2,25 @@
 import sys
 import os
 import os.path as op
+import pytest
 import tempfile
 import shutil
+import subprocess
 import datalad.api as dlapi
 sys.path.append("..")
 from babs.utils import (read_yaml)   # noqa
 
+# =============== Define several constant variables: ==================
 __location__ = os.path.realpath(
     os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
-WORKING_DIR = tempfile.mkdtemp()   # will be different each time you call it
+# make a working dir:
+WORKING_DIR = tempfile.mkdtemp()    # will be different each time you call it
+# if using `tempfile.mkdtemp()`, have to manually delete the temp folder!
+# if using `tempfile.TemporaryDirectory()`, it will be removed, even when pytest is failed.
+
 INPUT_DATA_BIDS_DIR = op.join(WORKING_DIR, "DSCSDSI")
+ORIGIN_CONTAINER_DS = op.join(WORKING_DIR, "my-container")
 # path of input datasets:
 ORIGIN_INPUT_DATA = read_yaml(op.join(__location__, "origin_input_dataset.yaml"))
 INFO_2ND_INPUT_DATA = {
@@ -20,10 +28,10 @@ INFO_2ND_INPUT_DATA = {
     # "type_session": this should be consistent with the first dataset
     "if_input_local": False
 }
-
-def pytest_sessionfinish(session, exitstatus):
-    """ whole test run finishes. """
-    shutil.rmtree(WORKING_DIR)
+LIST_WHICH_BIDSAPP = ["toybidsapp", "fmriprep", "qsiprep"]
+TOYBIDSAPP_VERSION = "0.0.6"
+TOYBIDSAPP_VERSION_DASH = TOYBIDSAPP_VERSION.replace(".", "-")
+# ====================================================================
 
 def get_input_data(which_input, type_session, if_input_local):
     """
@@ -55,16 +63,109 @@ def get_input_data(which_input, type_session, if_input_local):
 
     return path_in
 
+@pytest.fixture(scope="session")
+def if_singularity_installed():
+    if_singularity_installed = if_command_installed("singularity")
+    # also check if Docker is installed:
+    if_docker_installed = if_command_installed("docker")
+    # check if one of `singularity` and `docker` is installed:
+    if (not if_singularity_installed) and (not if_docker_installed):
+        raise Exception("Neither singularity or docker is installed!")
 
-def get_container_data(which_bidsapp, if_local=True):
+    return if_singularity_installed
+
+@pytest.fixture(scope="session")
+def prep_container_ds_toybidsapp(if_singularity_installed):
     """
-    This is to get path to container datalad dataset.
+    This is to pull toy BIDS App container image + create a datalad dataset of it.
+        Depending on if singularity is installed (True on CircleCI and clusters),
+    it will use singularity to build a sif file, or directly pull the Docker image.
+        Warning: no matter which BIDS App, we'll use toy BIDS App as the image,
+    and name the container as those in `LIST_WHICH_BIDSAPP`. Tested that the same image
+    can have different names in one datalad dataset.
 
     Parameters:
-    -----------
-    which_bidsapp: str
-        which BIDS App it is.
-    if_local: bool
-        if the container dataset is local [True] or remote (e.g., on OSF) [False]
+    --------------
+    if_singularity_installed: from a fixture; bool
+        True or False
+
+    # Returns:
+    # -----------
+    # fn_sif: str or None
+    #     The path to the built sif file.
+    #     If singularity is not installed, it will be `None`.
     """
-    print("TODO")
+    docker_addr = "pennlinc/toy_bids_app:" + TOYBIDSAPP_VERSION
+    docker_url = "docker://" + docker_addr
+
+    # Pull the container image:
+    if if_singularity_installed:
+        # build a singularity sif image:
+        filename_sif = "toybidsapp_" + TOYBIDSAPP_VERSION + ".sif"
+        fn_sif = op.join(WORKING_DIR, filename_sif)
+        cmd = "singularity build " + filename_sif + " " + docker_url
+        proc_build_sif = subprocess.run(
+            cmd.split(),
+            cwd=WORKING_DIR)
+        proc_build_sif.check_returncode()
+        # assert the sif file exists:
+        assert op.exists(fn_sif)
+    else:
+        # directly pull from docker:
+        cmd = "docker pull " + docker_addr
+        proc_docker_pull = subprocess.run(
+            cmd.split(),
+            cwd=WORKING_DIR)
+        proc_docker_pull.check_returncode()
+        fn_sif = None
+
+    # Set up container datalad dataset taht holds several names of containers
+    #   though all of them are toy BIDS App...
+    # create a new datalad dataset for holding the container:
+    container_ds_handle = dlapi.create(path=ORIGIN_CONTAINER_DS)
+    # add container image into this datalad dataset:
+    for which_bidsapp in LIST_WHICH_BIDSAPP:
+        if if_singularity_installed:   # add the sif file:
+            # datalad containers-add --url ${fn_sif} toybidsapp-${version_tag_dash}
+            # API help: in python env: `help(dlapi.containers_add)`
+            container_ds_handle.containers_add(
+                name=which_bidsapp+"-"+TOYBIDSAPP_VERSION_DASH,  # e.g., "toybidsapp-0-0-6"
+                url=fn_sif)
+            # can remove the original sif file now:
+            os.remove(fn_sif)
+        else:   # add docker image:
+            # datalad containers-add --url dhub://pennlinc/toy_bids_app:${version_tag} \
+            #   toybidsapp-${version_tag_dash}
+            container_ds_handle.containers_add(
+                name=which_bidsapp+"-"+TOYBIDSAPP_VERSION_DASH,  # e.g., "toybidsapp-0-0-6"
+                url="dhub://"+docker_addr   # e.g., "dhub://pennlinc/toy_bids_app:0.0.6"
+            )
+
+    return 0
+
+def if_command_installed(cmd):
+    """
+    This is to check if a command has been installed on the system
+
+    Parameters:
+    ------------
+    cmd: str
+        the command you want to test. e.g., 'singularity'
+
+    Returns:
+    ---------
+    if_installed: bool
+        True or False
+    """
+    a = shutil.which(cmd)
+    if a is None:   # not exist:
+        if_installed = False
+    else:
+        if_installed = True
+
+    return if_installed
+
+def pytest_sessionfinish(session, exitstatus):
+    """ whole test run finishes. """
+    # if using `tempfile.TemporaryDirectory`, then no need to manually remove this.
+    shutil.rmtree(WORKING_DIR)
--- tests/test_babs_init.py
+++ tests/test_babs_init.py
@@ -1,9 +1,17 @@
 # This is to test `babs-init`.
-
+import os
+import os.path as op
 import pytest
+import shutil
+import datalad.api as dlapi
 from get_data import (
     get_input_data,
-    INFO_2ND_INPUT_DATA
+    if_singularity_installed,
+    prep_container_ds_toybidsapp,
+    WORKING_DIR,
+    INFO_2ND_INPUT_DATA,
+    ORIGIN_CONTAINER_DS,
+    LIST_WHICH_BIDSAPP
 )
 
 # Define fixtures of different cases to test
@@ -13,22 +21,24 @@ from get_data import (
 # ARRANGE
 
 @pytest.mark.parametrize(
-    "which_bidsapp,which_input,type_session,if_input_local",
+    "which_bidsapp,which_input,type_session,if_input_local,if_two_input",
     # test toybidsapp: BIDS/zipped x single/multi-ses:
     #   the input data will also be remote by default:
-    [("toybidsapp", "BIDS", "single-ses", False),
-    #  ("toybidsapp", "BIDS", "multi-ses", False),
-    #  ("toybidsapp", "zipped_derivatives_qsiprep", "single-ses", False),
-    #  ("toybidsapp", "zipped_derivatives_qsiprep", "multi-ses", False),
+    [("toybidsapp", "BIDS", "single-ses", False, False),
+    #  ("toybidsapp", "BIDS", "multi-ses", False, False),
+    #  ("toybidsapp", "zipped_derivatives_qsiprep", "single-ses", False, False),
+    #  ("toybidsapp", "zipped_derivatives_qsiprep", "multi-ses", False, False),
     #  # test if input is local:
-    #  ("toybidsapp", "BIDS", "single-ses", True),
+    #  ("toybidsapp", "BIDS", "single-ses", True, False),
     #  # test fmriprep: single/multi-ses
-    #  ("fmriprep", "BIDS", "single-ses", False),
-    #  ("fmriprep", "BIDS", "multi-ses", False),
-    #  # test 2 input datasets:   # HOW TO DO SO????
-    #  ("toybidsapp_2inputs", "BIDS", "single-ses", False),
+    #  ("fmriprep", "BIDS", "single-ses", False, False),
+    #  ("fmriprep", "BIDS", "multi-ses", False, False),
+    #  # test 2 input datasets:
+    #  ("toybidsapp", "BIDS", "single-ses", False, True),
      ])
-def test_babs_init(which_bidsapp, which_input, type_session, if_input_local, tmp_path):
+def test_babs_init(which_bidsapp, which_input, type_session, if_input_local, if_two_input,
+                   tmp_path, prep_container_ds_toybidsapp,
+                   if_singularity_installed):
     """
     This is to test `babs-init` in different cases.
 
@@ -38,6 +48,7 @@ def test_babs_init(which_bidsapp, which_input, type_session, if_input_local, tmp
         The name of a BIDS App. However here we only use `toybidsapp` to test, even though you
         specified e.g., fmriprep; we'll make sure the BIDS App to be tested is reflected in
         `container_name` which BABS cares.
+        It must be one of the string in `LIST_WHICH_BIDSAPP`.
     which_input: str
         which input dataset. Options see keys in `origin_input_dataset.yaml`
     type_session: str
@@ -47,11 +58,13 @@ def test_babs_init(which_bidsapp, which_input, type_session, if_input_local, tmp
     tmp_path: str
         Path to a temporary directory, created by pytest
     """
+    # Sanity checks:
+    assert which_bidsapp in LIST_WHICH_BIDSAPP
 
     # Get the path to input dataset:
     path_in = get_input_data(which_input, type_session, if_input_local)
     input_ds_cli = [[path_in, which_input]]
-    if which_bidsapp == "toybidsapp_2inputs":
+    if if_two_input:
         # get another input dataset: qsiprep derivatives
         assert INFO_2ND_INPUT_DATA["which_input"] != which_input   # avoid repeated input ds name
         path_in_2nd = get_input_data(
@@ -60,8 +73,8 @@ def test_babs_init(which_bidsapp, which_input, type_session, if_input_local, tmp
             INFO_2ND_INPUT_DATA["if_input_local"])
         input_ds_cli.append([path_in_2nd], INFO_2ND_INPUT_DATA["which_input"])
 
-    # Get the path to the container dataset (depending on `which_case`):
-    print("TODO")
+    # Container dataset - has been set up by fixture `prep_container_ds_toybidsapp()`
+    assert op.exists(ORIGIN_CONTAINER_DS)
 
     # get the cli
     print("TODO")
@@ -84,5 +97,15 @@ def test_babs_init(which_bidsapp, which_input, type_session, if_input_local, tmp
 
     # anything else from `babs-check-setup`?
 
-    # clean up the temporary dir
-    print("TODO")
+    # Clean up the temporary dir:
+    # remove input dataset(s):
+    dlapi.remove(path=path_in)
+    if if_two_input:
+        dlapi.remove(dataset=path_in_2nd)
+    # remove container dataset:
+    dlapi.remove(dataset=ORIGIN_CONTAINER_DS, reckless="availability")
+    # ^^ --reckless is probably needed as there is no other container ds copy
+
+    # now, we can remove working directory:
+    shutil.rmtree(WORKING_DIR)
+    assert not op.exists(WORKING_DIR)
