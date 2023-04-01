cafaf8a6c824240b06862d213d8fa6da6af21b6b
Chenying Zhao <chenyzh@seas.upenn.edu>
2023-03-31 22:10:22 -0400

    add: running pytest on cluster: use singularity command to build toybidsapp sif file

--- tests/get_data.py
+++ tests/get_data.py
@@ -76,17 +76,6 @@ def get_input_data(which_input, type_session, if_input_local, tmp_path_factory):
     return path_in
 
 @pytest.fixture(scope="session")
-def if_singularity_installed():
-    if_singularity_installed = if_command_installed("singularity")
-    # also check if Docker is installed:
-    if_docker_installed = if_command_installed("docker")
-    # check if one of `singularity` and `docker` is installed:
-    if (not if_singularity_installed) and (not if_docker_installed):
-        raise Exception("Neither singularity or docker is installed!")
-
-    return if_singularity_installed
-
-@pytest.fixture(scope="session")
 def if_circleci():
     """ If it's currently running on CircleCI """
     env_circleci = os.getenv('CIRCLECI')    # a string 'true' or None
@@ -98,7 +87,33 @@ def if_circleci():
     return if_circleci
 
 @pytest.fixture(scope="session")
-def container_ds_path(if_circleci, tmp_path_factory):
+def where_now(if_circleci):
+    """
+    Determine where this pytest is running.
+    - if is on CircleCI: 'on_circleci';
+    - if not on CircleCI, but `singularity` is installed: 'on_cluster'
+    - if not either way, `docker` must be installed: 'on_local'
+    """
+    where_now = ""
+    # check if singularity is installed:
+    if_singularity_installed = if_command_installed("singularity")
+    # check if docker is installed:
+    if_docker_installed = if_command_installed("docker")
+
+    if if_circleci:
+        where_now = "on_circleci"
+    elif if_singularity_installed:
+        where_now = "on_cluster"
+    elif if_docker_installed:
+        where_now = "on_local"
+    else:
+        where_now = ""
+        raise Exception("Not on CircleCI, and neither singularity nor docker is installed!"
+                        + " Pytest cannot proceed.")
+    return where_now
+
+@pytest.fixture(scope="session")
+def container_ds_path(where_now, tmp_path_factory):
     """
     This is to get toy BIDS App container image + create a datalad dataset of it.
         Depending on if pytest is running on CircleCI,
@@ -109,9 +124,11 @@ def container_ds_path(if_circleci, tmp_path_factory):
 
     Parameters:
     --------------
-    if_circleci: from a fixture; bool
-        If it's on circle ci. If so, will use pre-built sif file of toybidsapp stored in
-        the docker image used for BABS tests.
+    where_now: from a fixture; str or empty str ("")
+        Depending on if on circleci, and singularity or docker is installed:
+        - "on_circleci": will use pre-built sif file;
+        - "on_cluster": will use `singularity` command to build the sif file
+        - "on_local": will use `docker` to pull the container
     tmp_path_factory: fixture in pytest
 
     Returns:
@@ -120,17 +137,33 @@ def container_ds_path(if_circleci, tmp_path_factory):
         path to the created container datalad dataset
     """
     docker_addr = "pennlinc/toy_bids_app:" + TOYBIDSAPP_VERSION
+    docker_url = "docker://" + docker_addr
 
     # Pull the container image:
-    if if_circleci:
+    if where_now == "on_circleci":
         # assert the sif file exists:
         assert op.exists(FN_TOYBIDSAPP_SIF_CIRCLECI)
-    else:
+        fn_toybidsapp_sif = FN_TOYBIDSAPP_SIF_CIRCLECI
+    elif where_now == "on_cluster":
+        # build the sif file:
+        folder_toybidsapp_sif = tmp_path_factory.mktemp("temp_singularity_images")
+        fn_toybidsapp_sif = op.join(folder_toybidsapp_sif,
+                                    "toybidsapp_" + TOYBIDSAPP_VERSION + ".sif")
+        proc_singularity_build = subprocess.run(
+            # singularity build toybidsapp_${toybidsapp_version}.sif \
+            #       docker://pennlinc/toy_bids_app:${toybidsapp_version}
+            ["singularity", "build", fn_toybidsapp_sif, docker_url],
+            stdout=subprocess.PIPE)
+        proc_singularity_build.check_returncode()
+    elif where_now == "on_local":
         # directly pull from docker:
         cmd = "docker pull " + docker_addr
         proc_docker_pull = subprocess.run(
             cmd.split())
         proc_docker_pull.check_returncode()
+        fn_toybidsapp_sif = None
+    else:
+        raise Exception("Invalid `where_now` = '" + where_now + "'!")
 
     # Set up container datalad dataset that holds several names of containers
     #   though all of them are toy BIDS App...
@@ -141,15 +174,15 @@ def container_ds_path(if_circleci, tmp_path_factory):
     container_ds_handle = dlapi.create(path=origin_container_ds)
     # add container image into this datalad dataset:
     for which_bidsapp in LIST_WHICH_BIDSAPP:
-        if if_circleci:   # add the sif file:
+        if where_now in ["on_circleci", "on_cluster"]:   # add the built sif file:
             # datalad containers-add --url ${fn_sif} toybidsapp-${version_tag_dash}
             # API help: in python env: `help(dlapi.containers_add)`
             container_ds_handle.containers_add(
                 name=which_bidsapp+"-"+TOYBIDSAPP_VERSION_DASH,  # e.g., "toybidsapp-0-0-6"
-                url=FN_TOYBIDSAPP_SIF_CIRCLECI)
+                url=fn_toybidsapp_sif)
             # # can remove the original sif file now:
-            # os.remove(FN_TOYBIDSAPP_SIF_CIRCLECI)
-        else:   # add docker image:
+            # os.remove(fn_toybidsapp_sif)
+        elif where_now == "on_local":   # add docker image:
             # datalad containers-add --url dhub://pennlinc/toy_bids_app:${version_tag} \
             #   toybidsapp-${version_tag_dash}
             container_ds_handle.containers_add(
--- tests/test_babs_check_setup.py
+++ tests/test_babs_check_setup.py
@@ -13,6 +13,7 @@ from babs.cli import (    # noqa
 from get_data import (   # noqa
     get_input_data,
     container_ds_path,
+    where_now,
     if_circleci,
     __location__,
     INFO_2ND_INPUT_DATA,
