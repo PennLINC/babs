d41056ac051b5c8ad81f675f5221aa13a7f9473e
Chenying Zhao <chenyzh@seas.upenn.edu>
2023-04-18 10:18:48 -0400

    release setuptools version restrictions; not code the version number in scripts

--- babs/__init__.py
+++ babs/__init__.py
@@ -1,7 +1,8 @@
 """Top-level package for BABS."""
+from babs._version import get_versions
 
 __author__ = """PennLINC"""
 __email__ = 'PennLINC@gmail.com'
-__version__ = '0.0.1'
+__version__ = get_versions()["version"]
 
 # from .babs import babs
