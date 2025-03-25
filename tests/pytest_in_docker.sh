9db9855c2fd6d1c1bf0f8f711e19a1bc525f01bf
Matt Cieslak <mattcieslak@gmail.com>
2025-03-25 13:12:12 -0400

    add shellcheck to ci image

---
+++ tests/pytest_in_docker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd ..
+docker build -t pennlinc/slurm-docker-ci:unstable -f Dockerfile_testing .
+docker run -it \
+    -h slurmctl --cap-add sys_admin \
+    --privileged \
+    pennlinc/slurm-docker-ci:unstable pytest -svx --pdb \
+    /babs/tests
