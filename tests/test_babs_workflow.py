08b52ab9a68ba6de6beb39bf372840012368de67
mattcieslak <mattcieslak@gmail.com>
2025-04-21 17:30:40 -0400

    add back workflow test

--- tests/test_babs_workflow.py
+++ tests/test_babs_workflow.py
@@ -1,130 +1,139 @@
 """Test the babs workflow."""
 
+import argparse
+import os
 import os.path as op
+import time
 from pathlib import Path
+from unittest import mock
 
 import pytest
+from conftest import get_config_simbids_path, update_yaml_for_run
 
-# @pytest.mark.parametrize('processing_level', ['subject', 'session'])
-# def test_babs_init_raw_bids(
-#     tmp_path_factory,
-#     templateflow_home,
-#     bids_data_singlesession,
-#     bids_data_multisession,
-#     processing_level,
-#     simbids_container_ds,
-# ):
-#     """
-#     This is to test `babs init` on raw BIDS data.
-#     """
+from babs.cli import _enter_check_setup, _enter_init, _enter_merge, _enter_status, _enter_submit
+from babs.utils import squeue_to_pandas
 
-#     # Check the container dataset
-#     assert op.exists(simbids_container_ds)
-#     assert op.exists(op.join(simbids_container_ds, '.datalad/config'))
 
-#     # Check the bids input dataset:
-#     assert op.exists(bids_data_singlesession)
-#     assert op.exists(op.join(bids_data_singlesession, '.datalad/config'))
+@pytest.mark.parametrize('processing_level', ['subject', 'session'])
+def test_babs_init_raw_bids(
+    tmp_path_factory,
+    templateflow_home,
+    bids_data_singlesession,
+    bids_data_multisession,
+    processing_level,
+    simbids_container_ds,
+):
+    """
+    This is to test `babs init` on raw BIDS data.
+    """
 
-#     # Preparation of env variable `TEMPLATEFLOW_HOME`:
-#     os.environ['TEMPLATEFLOW_HOME'] = str(templateflow_home)
-#     assert os.getenv('TEMPLATEFLOW_HOME')
+    # Check the container dataset
+    assert op.exists(simbids_container_ds)
+    assert op.exists(op.join(simbids_container_ds, '.datalad/config'))
 
-#     # Get the cli of `babs init`:
-#     project_base = tmp_path_factory.mktemp('project')
-#     project_root = project_base / 'my_babs_project'
-#     container_name = 'simbids-0-0-3'
+    # Check the bids input dataset:
+    assert op.exists(bids_data_singlesession)
+    assert op.exists(op.join(bids_data_singlesession, '.datalad/config'))
 
-#     # Use config_simbids.yaml instead of eg_fmriprep
-#     config_simbids_path = get_config_simbids_path()
-#     container_config = update_yaml_for_run(
-#         project_base,
-#         config_simbids_path.name,
-#         {
-#             'BIDS': bids_data_singlesession
-#             if processing_level == 'subject'
-#             else bids_data_multisession
-#         },
-#     )
+    # Preparation of env variable `TEMPLATEFLOW_HOME`:
+    os.environ['TEMPLATEFLOW_HOME'] = str(templateflow_home)
+    assert os.getenv('TEMPLATEFLOW_HOME')
 
-#     babs_init_opts = argparse.Namespace(
-#         project_root=project_root,
-#         list_sub_file=None,
-#         container_ds=simbids_container_ds,
-#         container_name=container_name,
-#         container_config=container_config,
-#         processing_level=processing_level,
-#         queue='slurm',
-#         keep_if_failed=False,
-#     )
+    # Get the cli of `babs init`:
+    project_base = tmp_path_factory.mktemp('project')
+    project_root = project_base / 'my_babs_project'
+    container_name = 'simbids-0-0-3'
 
-#     # Test error when project root already exists
-#     project_root.mkdir(parents=True, exist_ok=True)
-#     with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_init_opts):
-#         with pytest.raises(FileExistsError, match=r'already exists'):
-#             _enter_init()
+    # Use config_simbids.yaml instead of eg_fmriprep
+    config_simbids_path = get_config_simbids_path()
+    container_config = update_yaml_for_run(
+        project_base,
+        config_simbids_path.name,
+        {
+            'BIDS': bids_data_singlesession
+            if processing_level == 'subject'
+            else bids_data_multisession
+        },
+    )
 
-#     # Test error when parent directory doesn't exist
-#     non_existent_parent = project_base / 'non_existent' / 'my_babs_project'
-#     babs_init_opts.project_root = non_existent_parent
-#     with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_init_opts):
-#         with pytest.raises(ValueError, match=r'parent folder.*does not exist'):
-#             _enter_init()
+    babs_init_opts = argparse.Namespace(
+        project_root=project_root,
+        list_sub_file=None,
+        container_ds=simbids_container_ds,
+        container_name=container_name,
+        container_config=container_config,
+        processing_level=processing_level,
+        queue='slurm',
+        keep_if_failed=False,
+    )
 
-#     # Test error when parent directory doesn't exist
-#     babs_init_opts.project_root = project_root
-#     with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_init_opts):
-#         _enter_init()
+    # Test error when project root already exists
+    project_root.mkdir(parents=True, exist_ok=True)
+    with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_init_opts):
+        with pytest.raises(FileExistsError, match=r'already exists'):
+            _enter_init()
 
-#     # babs check-setup:
-#     babs_check_setup_opts = argparse.Namespace(project_root=project_root, job_test=True)
-#     with mock.patch.object(
-#         argparse.ArgumentParser, 'parse_args', return_value=babs_check_setup_opts
-#     ):
-#         _enter_check_setup()
+    # Test error when parent directory doesn't exist
+    non_existent_parent = project_base / 'non_existent' / 'my_babs_project'
+    babs_init_opts.project_root = non_existent_parent
+    with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_init_opts):
+        with pytest.raises(ValueError, match=r'parent folder.*does not exist'):
+            _enter_init()
 
-#     # test babs status before submitting jobs
-#     babs_status_opts = argparse.Namespace(project_root=project_root)
-#     with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_status_opts):
-#         _enter_status()
+    # Test error when parent directory doesn't exist
+    babs_init_opts.project_root = project_root
+    with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_init_opts):
+        _enter_init()
 
-#     # babs submit:
-#     babs_submit_opts = argparse.Namespace(
-#         project_root=project_root, select=None, inclusion_file=None, count=1
-#     )
-#     with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_submit_opts):
-#         _enter_submit()
+    # babs check-setup:
+    babs_check_setup_opts = argparse.Namespace(project_root=project_root, job_test=True)
+    with mock.patch.object(
+        argparse.ArgumentParser, 'parse_args', return_value=babs_check_setup_opts
+    ):
+        _enter_check_setup()
 
-#     # babs status:
-#     babs_status_opts = argparse.Namespace(project_root=project_root)
-#     with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_status_opts):
-#         _enter_status()
+    # test babs status before submitting jobs
+    babs_status_opts = argparse.Namespace(project_root=project_root)
+    with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_status_opts):
+        _enter_status()
 
-#     finished = False
-#     for waitnum in [5, 8, 10, 15, 30, 60]:
-#         time.sleep(waitnum)
-#         print(f'Waiting {waitnum} seconds...')
-#         df = squeue_to_pandas()
-#         print(df)
-#         if df.empty:
-#             finished = True
-#             break
+    # babs submit:
+    babs_submit_opts = argparse.Namespace(
+        project_root=project_root, select=None, inclusion_file=None, count=1
+    )
+    with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_submit_opts):
+        _enter_submit()
 
-#     if not finished:
-#         raise RuntimeError('Jobs did not finish in time')
+    # babs status:
+    babs_status_opts = argparse.Namespace(project_root=project_root)
+    with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_status_opts):
+        _enter_status()
 
-#     # Submit the last job:
-#     babs_submit_opts = argparse.Namespace(
-#         project_root=project_root, select=None, inclusion_file=None, count=None
-#     )
-#     with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_submit_opts):
-#         _enter_submit()
+    finished = False
+    for waitnum in [5, 8, 10, 15, 30, 60]:
+        time.sleep(waitnum)
+        print(f'Waiting {waitnum} seconds...')
+        df = squeue_to_pandas()
+        print(df)
+        if df.empty:
+            finished = True
+            break
 
-#     babs_merge_opts = argparse.Namespace(
-#         project_root=project_root, chunk_size=2000, trial_run=False
-#     )
-#     with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_merge_opts):
-#         _enter_merge()
+    if not finished:
+        raise RuntimeError('Jobs did not finish in time')
+
+    # Submit the last job:
+    babs_submit_opts = argparse.Namespace(
+        project_root=project_root, select=None, inclusion_file=None, count=None
+    )
+    with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_submit_opts):
+        _enter_submit()
+
+    babs_merge_opts = argparse.Namespace(
+        project_root=project_root, chunk_size=2000, trial_run=False
+    )
+    with mock.patch.object(argparse.ArgumentParser, 'parse_args', return_value=babs_merge_opts):
+        _enter_merge()
 
 
 def test_bootstrap_cleanup(babs_project_sessionlevel_babsobject):
